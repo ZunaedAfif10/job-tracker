4cc442f8fd3ba0c2b0c956ba04aaf782526dfd26
ZunaedAfif10 <zunaedrahman7@gmail.com>
2026-02-24 19:56:50 +0600

    completed delete button functionality

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 // list
 let interviewList = [];
 let rejectList = [];
+let allList = [];
+
+
 
 let stat = 'all-cards';
 
@@ -8,8 +11,27 @@ let stat = 'all-cards';
 let totalCount = document.getElementById('total');
 let interviewCount = document.getElementById('interview-count');
 let rejectCount = document.getElementById('reject-count');
+let sidebar = document.getElementById('sidebar');
 
-const allCard = document.getElementById('all-cards');
+// console.log(allList);
+let allCard = document.getElementById('all-cards');
+allList = allCard.querySelectorAll('.company');
+
+
+function cardDelete(company) {
+    
+    // console.log(company);
+    for (card of allList) {
+        // console.log(card);
+        if (card.innerText === company) {
+            prevNode = card.parentNode.parentNode.parentNode;
+            // console.log(prevNode);
+            prevNode.remove();
+            break;
+        }
+    }
+}
+
 
 
 // calculating total
@@ -17,9 +39,11 @@ function calculate() {
     totalCount.innerText = allCard.children.length;
     interviewCount.innerText = interviewList.length;
     rejectCount.innerText = rejectList.length;
+    sidebar.innerText = totalCount.innerText + ' jobs';
 }
 
 calculate();
+
 
 // dynamic button
 const allBtn = document.getElementById('all-btn');
@@ -42,38 +66,34 @@ btnSec.addEventListener('click', function (event) {
     event.target.classList.remove("text-gray-500", "bg-white")
     event.target.classList.add("text-white", "bg-primary");
 
-    if(event.target === interviewBtn)
-    {
+    if (event.target === interviewBtn) {
         filterSec.classList.remove('hidden');
         allCard.classList.add('hidden');
-        if(interviewList.length === 0)
-        {
+        sidebar.innerText = interviewCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        if (interviewList.length === 0) {
             noJob.classList.remove('hidden');
         }
-        else if(interviewList.length !== 0)
-        {
+        else if (interviewList.length !== 0) {
             noJob.classList.add('hidden');
         }
         renderInterview();
     }
-    else if(event.target === rejectBtn)
-    {
+    else if (event.target === rejectBtn) {
         filterSec.classList.remove('hidden');
         allCard.classList.add('hidden');
-        if(rejectList.length === 0)
-        {
+        sidebar.innerText = rejectCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        if (rejectList.length === 0) {
             noJob.classList.remove('hidden');
         }
-        else if(rejectList.length !== 0)
-        {
+        else if (rejectList.length !== 0) {
             noJob.classList.add('hidden');
         }
         renderReject();
     }
-    else if(event.target === allBtn)
-    {
+    else if (event.target === allBtn) {
         filterSec.classList.add('hidden');
         allCard.classList.remove('hidden');
+        sidebar.innerText = totalCount.innerText + ' jobs';
     }
 
 })
@@ -83,16 +103,16 @@ btnSec.addEventListener('click', function (event) {
 const mainContain = document.getElementById('mainContainer');
 
 mainContain.addEventListener('click', function (event) {
-    
+    // console.log(event.target.classList);
     if (event.target.classList.contains('interview-main')) {
         const parNode = event.target.parentNode.parentNode;
-        
+
         const company = parNode.querySelector('.company').innerText;
         const skill = parNode.querySelector('.skill').innerText;
         const payment = parNode.querySelector('.payment').innerText;
         const st = parNode.querySelector('.st').innerText;
         const describe = parNode.querySelector('.describe').innerText;
-        
+
         const cardInfo = {
             company,
             skill,
@@ -100,38 +120,39 @@ mainContain.addEventListener('click', function (event) {
             st: 'INTERVIEW',
             describe
         }
-        
-        parNode.querySelector('.st').innerText ='INTERVIEW';
+
+        parNode.querySelector('.st').innerText = 'INTERVIEW';
         parNode.querySelector('.st').className = 'st border-green-500 text-green-500 border-2 font-semibold py-2 px-3 rounded-md inline-block mb-2';
         const cardExist = interviewList.find(item => item.company === cardInfo.company);
         if (!cardExist) {
             interviewList.push(cardInfo);
         }
         rejectList = rejectList.filter(item => item.company != cardInfo.company);
-        if(stat === 'reject-btn')
-            {
-                renderReject();
-                if(rejectList.length === 0)
-                {
-                    noJob.classList.remove('hidden');
-                }
-                else if(rejectList.length !== 0)
-                {
-                    noJob.classList.add('hidden');
-                }
-        }
         calculate();
-        
+        if (stat === 'reject-btn') {
+            renderReject();
+            if (rejectList.length === 0) {
+                noJob.classList.remove('hidden');
+            }
+            else if (rejectList.length !== 0) {
+                noJob.classList.add('hidden');
+            }
+            sidebar.innerText = rejectCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+        if (stat === 'interview-btn') {
+            sidebar.innerText = interviewCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+
     }
     else if (event.target.classList.contains('reject-main')) {
         const parNode = event.target.parentNode.parentNode;
-        
+
         const company = parNode.querySelector('.company').innerText;
         const skill = parNode.querySelector('.skill').innerText;
         const payment = parNode.querySelector('.payment').innerText;
         const st = parNode.querySelector('.st').innerText;
         const describe = parNode.querySelector('.describe').innerText;
-        
+
         const cardInfo = {
             company,
             skill,
@@ -139,29 +160,75 @@ mainContain.addEventListener('click', function (event) {
             st: 'REJECT',
             describe
         }
-        
-        parNode.querySelector('.st').innerText ='REJECT';
+
+        parNode.querySelector('.st').innerText = 'REJECT';
         parNode.querySelector('.st').className = 'st border-red-500 text-red-500 border-2 font-semibold py-2 px-3 rounded-md inline-block mb-2';
         const cardExist = rejectList.find(item => item.company === cardInfo.company);
         if (!cardExist) {
             rejectList.push(cardInfo);
         }
         interviewList = interviewList.filter(item => item.company != cardInfo.company);
-        if(stat === 'interview-btn')
-            {
-                renderInterview();
-                if(interviewList.length === 0)
-                {
-                    noJob.classList.remove('hidden');
-                }
-                else if(interviewList.length !== 0)
-                {
-                    noJob.classList.add('hidden');
-                }
-        }
         calculate();
-        
-        
+        if (stat === 'interview-btn') {
+            renderInterview();
+            if (interviewList.length === 0) {
+                noJob.classList.remove('hidden');
+            }
+            else if (interviewList.length !== 0) {
+                noJob.classList.add('hidden');
+            }
+            sidebar.innerText = interviewCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+        if (stat === 'reject-btn') {
+            sidebar.innerText = rejectCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+
+
+    }
+    else if (event.target.closest('.delete')) {
+        // console.log(event.target.id);
+        const parNode = event.target.parentNode.parentNode;
+
+        const company = parNode.querySelector('.company').innerText;
+
+        const cardInfo = {
+            company
+        }
+        parNode.remove();
+
+        interviewList = interviewList.filter(item => item.company != cardInfo.company);
+        rejectList = rejectList.filter(item => item.company != cardInfo.company);
+
+        calculate();
+        if (stat === 'reject-btn') {
+            renderReject();
+            cardDelete(company);
+            calculate();
+            if (rejectList.length === 0) {
+                noJob.classList.remove('hidden');
+            }
+            else if (rejectList.length !== 0) {
+                noJob.classList.add('hidden');
+            }
+            sidebar.innerText = rejectCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+
+        if (stat === 'interview-btn') {
+            renderInterview();
+            cardDelete(company);
+            calculate();
+            if (interviewList.length === 0) {
+                noJob.classList.remove('hidden');
+            }
+            else if (interviewList.length !== 0) {
+                noJob.classList.add('hidden');
+            }
+            sidebar.innerText = interviewCount.innerText + ' of ' + totalCount.innerText + ' jobs';
+        }
+
+
+
+
     }
 })
 
@@ -192,8 +259,8 @@ function renderInterview() {
                     </div>
                 </div>
                 <div>
-                    <button class="btn border-2 border-gray-100 px-[10px] rounded-full"><i
-                            class="fa-regular fa-trash-can text-gray-400"></i></button>
+                    <button class="btn delete border-2 border-gray-100 px-[10px] rounded-full"><i
+                            class="fa-regular pointer-events-none fa-trash-can text-gray-400"></i></button>
                 </div>
         `
         filterSec.appendChild(div);
@@ -226,8 +293,8 @@ function renderReject() {
                     </div>
                 </div>
                 <div>
-                    <button class="btn border-2 border-gray-100 px-[10px] rounded-full"><i
-                            class="fa-regular fa-trash-can text-gray-400"></i></button>
+                    <button class="btn delete border-2 border-gray-100 px-[10px] rounded-full"><i
+                            class="fa-regular pointer-events-none fa-trash-can text-gray-400"></i></button>
                 </div>
         `
         filterSec.appendChild(div);
